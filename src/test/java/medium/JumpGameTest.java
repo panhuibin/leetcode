907fd65edec9ef1bb8a1391770c30900d337b5f2
Sarah Pan <span@kobo.com>
2018-08-22 21:09:16 -0400

    new solutions

---
+++ src/test/java/medium/JumpGameTest.java
@@ -0,0 +1,14 @@
+package medium;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JumpGameTest {
+
+    JumpGame j = new JumpGame();
+
+    @Test
+    public void canJump() {
+    }
+}
