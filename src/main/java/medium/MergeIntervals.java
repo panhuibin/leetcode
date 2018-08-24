23c4f4608b0255d092ec4d039466fa15f7b347b4
Sarah Pan <span@kobo.com>
2018-08-23 22:36:57 -0400

    more solutions

--- src/main/java/medium/MergeIntervals.java
+++ src/main/java/medium/MergeIntervals.java
@@ -3,41 +3,60 @@ package medium;
 /**
  * https://leetcode.com/problems/merge-intervals/description/
  * Given a collection of intervals, merge all overlapping intervals.
-
- Example 1:
-
- Input: [[1,3],[2,6],[8,10],[15,18]]
- Output: [[1,6],[8,10],[15,18]]
- Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
- Example 2:
-
- Input: [[1,4],[4,5]]
- Output: [[1,5]]
- Explanation: Intervals [1,4] and [4,5] are considerred overlapping.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [[1,3],[2,6],[8,10],[15,18]]
+ * Output: [[1,6],[8,10],[15,18]]
+ * Explanation: Since intervals [1,3] and [2,6] overlaps, merge them into [1,6].
+ * Example 2:
+ * <p>
+ * Input: [[1,4],[4,5]]
+ * Output: [[1,5]]
+ * Explanation: Intervals [1,4] and [4,5] are considerred overlapping.
  */
-
-import module.Interval;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import module.Interval;
+
 /**
  * Definition for an interval.
  * public class Interval {
- *     int start;
- *     int end;
- *     Interval() { start = 0; end = 0; }
- *     Interval(int s, int e) { start = s; end = e; }
+ * int start;
+ * int end;
+ * Interval() { start = 0; end = 0; }
+ * Interval(int s, int e) { start = s; end = e; }
  * }
  */
 public class MergeIntervals {
     public List<Interval> merge(List<Interval> intervals) {
         List<Interval> sortedIntervals = new ArrayList<>();
-        for(Interval interval:intervals){
-            for(Interval intervalInArray: sortedIntervals){
-                if(ab)
+        for (Interval interval : intervals) {
+            List<Interval> intervalsToRemove = new ArrayList<>();
+            for (Interval intervalInArray : sortedIntervals) {
+                if (isOverlapping(intervalInArray, interval)) {
+                    interval = joinInterval(intervalInArray, interval);
+                    intervalsToRemove.add(intervalInArray);
+                }
             }
+            sortedIntervals.removeAll(intervalsToRemove);
+            sortedIntervals.add(interval);
         }
+        return sortedIntervals;
+    }
+
+    private boolean isOverlapping(Interval first, Interval second) {
+        if(first.end<second.start) return false;
+        else if(first.start>second.end) return false;
+        else return true;
+    }
+
+    private Interval joinInterval(Interval first, Interval second) {
+        int start = Math.min(first.start, second.start);
+        int end = Math.max(first.end, second.end);
+        return new Interval(start, end);
     }
 
 }
--- src/test/java/medium/MergeIntervalsTest.java
+++ src/test/java/medium/MergeIntervalsTest.java
@@ -20,7 +20,61 @@ public class MergeIntervalsTest {
         intervals.add(new Interval(8, 10));
         intervals.add(new Interval(15, 18));
         List<Interval> output = m.merge(intervals);
+        print(output);
+    }
 
+    @Test
+    public void merge2() {
+        List<Interval> intervals = new ArrayList<>();
+        intervals.add(new Interval(1, 10));
+        intervals.add(new Interval(2, 3));
+        intervals.add(new Interval(4, 6));
+        intervals.add(new Interval(3, 18));
+        List<Interval> output = m.merge(intervals);
+        print(output);
+    }
+
+    @Test
+    public void merge3() {
+        List<Interval> intervals = new ArrayList<>();
+        intervals.add(new Interval(1, 1));
+        intervals.add(new Interval(2, 2));
+        List<Interval> output = m.merge(intervals);
+        print(output);
+    }
+
+    @Test
+    public void merge4() {
+        List<Interval> intervals = new ArrayList<>();
+        intervals.add(new Interval(1, 1));
+        intervals.add(new Interval(1, 2));
+        List<Interval> output = m.merge(intervals);
+        print(output);
+    }
+
+    @Test
+    public void merge5() {
+        List<Interval> intervals = new ArrayList<>();
+        intervals.add(new Interval(1, 3));
+        intervals.add(new Interval(1, 2));
+        List<Interval> output = m.merge(intervals);
+        print(output);
+    }
+
+    @Test
+    public void merge6() {
+        List<Interval> intervals = new ArrayList<>();
+        List<Interval> output = m.merge(intervals);
+        print(output);
+    }
+
+    @Test
+    public void merge7() {
+        List<Interval> intervals = new ArrayList<>();
+        intervals.add(new Interval(1, 4));
+        intervals.add(new Interval(0, 0));
+        List<Interval> output = m.merge(intervals);
+        print(output);
     }
 
     private void print(List<Interval> intervals) {
