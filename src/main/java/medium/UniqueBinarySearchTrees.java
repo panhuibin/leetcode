aae1130f08f6045c6df7910c9bf0ea06a2c0bc21
Sarah Pan <span@kobo.com>
2018-09-09 16:30:53 -0400

    more solutions

--- src/main/java/medium/UniqueBinarySearchTrees.java
+++ src/main/java/medium/UniqueBinarySearchTrees.java
@@ -2,19 +2,19 @@ package medium;
 
 /**
  * Given n, how many structurally unique BST's (binary search trees) that store values 1 ... n?
- *
+ * <p>
  * Example:
- *
+ * <p>
  * Input: 3
  * Output: 5
  * Explanation:
  * Given n = 3, there are a total of 5 unique BST's:
- *
- *    1         3     3      2      1
- *     \       /     /      / \      \
- *      3     2     1      1   3      2
- *     /     /       \                 \
- *    2     1         2                 3
+ * <p>
+ * 1         3     3      2      1
+ * \       /     /      / \      \
+ * 3     2     1      1   3      2
+ * /     /       \                 \
+ * 2     1         2                 3
  */
 
 class UniqueBinarySearchTrees {
@@ -26,10 +26,22 @@ class UniqueBinarySearchTrees {
          *   |         /
          *    2       1
          * root = 1, 1.right = 2;
-         * so what happened? we tried to add 2 to the original tree, it can be added to the
+         * when n=3, there're 5 ways of tree.
+         * when 1 is root, left has 0 nodes and right has 2 nodes, there're dp[0]*dp[2] = 2 possibility
+         * when 2 is root, left has 1 node and right has 1 node, there're dp[1]*dp[1] = 1 possibility.
+         * when 3 is root, left has 2 nodes and right has 0 nodes, there're dp[2]*dp[0] = 2 possibility.
          */
 
-        return -1;
-
+        int[] dp = new int[n + 1]; //calculate dp[n] by all previous dp elements
+        dp[0] = 1;
+        dp[1] = 1;
+        //calculate dp[level] from dp[2]
+        for (int level = 2; level <= n; level++) {
+            //each need to calculate when each element are root
+            for (int root = 1; root <= level; root++) {
+                dp[level] += dp[level - root] * dp[root];
+            }
+        }
+        return dp[n];
     }
 }
